14e1ae2db3c8b939a5091f7076f93c1988a232af
Vardaan <vardaansethi007@gmail.com>
2023-04-08 17:22:33 +0530

    adding models for user and setting up web api project

---
+++ LoginWebAPI/LoginWebAPI/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginWebAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public string Role { get; set; }
+    }
+}
